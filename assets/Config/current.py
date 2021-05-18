24eb02b6b7b1a059033f51f7f5252f4c97dbcbde
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-18 19:32:26 -0400

    Create current.py

---
+++ assets/Config/current.py
@@ -0,0 +1,22 @@
+import os, sys, time
+
+class Current:
+    CurrentCmd = {
+        "Cmd": "",
+        "args": [],
+        "fullcmd": ""
+    }
+
+    CurrentInfo = {
+        "Username": "",
+        "IP": "",
+        "Port": "",
+        "Password": "",
+        "Level": "",
+        "Maxime": "",
+        "Admin": ""
+    }
+
+    CurrentLoginInfo = {
+
+    }
