c1aea41879f4cd5fd65277ea136a87f71202b135
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-18 19:32:49 -0400

    Create main.py

---
+++ assets/utils/main.py
@@ -0,0 +1,25 @@
+import os, sys, time
+
+class utils:
+    def set_Title(msg):
+        return ""
+
+    def set_Cursor(r, c):
+        return ""
+    
+    def moveLeft_Cursor():
+        return ""
+
+    def moveRight_Cursor():
+        return ""
+    
+    def moveUp_Cursor():
+        return
+
+    def moveDown_Cursor():
+        return
+
+    def removeRow():
+        return ""
+
+    
