8da100b4d0cc361834cebce08f127a99993dca99
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-18 19:32:23 -0400

    Create modify.py

---
+++ assets/banner_system/modify.py
@@ -0,0 +1,17 @@
+import sys, os, time
+
+
+class BannerModify:
+    global appOwner
+    global appInstagram
+    global appDiscord
+    global appDiscordServer
+    global appName
+    global appVersion
+    global Username
+    global IPAddr
+
+    def ColorBanner(bnnr):
+        print("")
+
+    
