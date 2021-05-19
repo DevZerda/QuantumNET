fb72016a347235d89166ac0859fab4631c0ee2c0
Exo0001 <84390579+Exo0001@users.noreply.github.com>
2021-05-19 18:35:17 +0100

    Update discord.py

--- assets/Logger/discord.py
+++ assets/Logger/discord.py
@@ -1,5 +1,6 @@
-import os, sys, time
-
-class Discord:
-    def send_msg():
-        print("")
+webhooks = {
+	"screen_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
+	"login_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
+	"action_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
+	"attack_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1"
+} # Exo
