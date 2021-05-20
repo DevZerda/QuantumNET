0f045f8e4ff1082456c95c72dbaafeef004ad985
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-20 15:35:04 -0400

    Update discord.py

--- assets/Logger/discord.py
+++ assets/Logger/discord.py
@@ -4,16 +4,16 @@ from discord_webhook import DiscordWebhook
 
 class Discord():
 	# webhooks = {
-	# 	"screen_logs": "https://discord.com/api/webhooks/844855350801072159/xPsomucP4neo4dOdGLBaIbRmz8ldvCr9617c2FcWgDjrwrvuemlc2shLtl5TJCjq6zi7",
-	# 	"login_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
-	# 	"action_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
-	# 	"attack_logs": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1"
+	# 	"screen": "https://discord.com/api/webhooks/844855350801072159/xPsomucP4neo4dOdGLBaIbRmz8ldvCr9617c2FcWgDjrwrvuemlc2shLtl5TJCjq6zi7",
+	# 	"login": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
+	# 	"action": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
+	# 	"attack": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1"
 	# } # Exo
 
 	def send_notice(msg):
-		# types = ["screen", "login", "attack", "log"]
+		# types = ["screen", "login", "attack", "action"]
 		# if type in types:
-		webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/844894832405381161/jCw_h5_NWkAPci0SyqMoW5Xh_NT9TxRR65BZqEgOOX9PyGyz0QOjGumoc1aeAcbWZQMO', content=msg)
+		webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/844855350801072159/xPsomucP4neo4dOdGLBaIbRmz8ldvCr9617c2FcWgDjrwrvuemlc2shLtl5TJCjq6zi7', content=msg)
 		webhook.execute()
 		# else: 
 			# print("Unable to send request!")
