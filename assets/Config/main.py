dc5bcbbe812418b7560bbe6ef2decf19c305b4bf
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-21 14:48:15 -0400

    Update main.py

--- assets/Config/main.py
+++ assets/Config/main.py
@@ -1,18 +1,27 @@
 
-MainColors = {
-    "Red": "\x1b[31m",
-	"Yellow": "\x1b[33m",
-	"Blue": "\x1b[34m",
-	"Purple": "\x1b[95m",
-	"Green": "\x1b[32m",
-	"Cyan": "\x1b[96m",
-	"Black": "\x1b[30m",
-	"Grey": "\x1b[90m",
-	"Reset": "\x1b[39m",
-	"Background_Red": "\x1b[41m",
-	"Background_Green": "\x1b[42m",
-	"Background_Grey": "\x1b[100m",
-	"Background_Reset": "\x1b[49m",
-	"Clear": "\033[2J\033[1;1H",
-	"hostname": "Quantum@Net:~# "
-}
+class Strings():
+	MainColors = {
+		"Red": "\x1b[31m",
+		"Yellow": "\x1b[33m",
+		"Blue": "\x1b[34m",
+		"Purple": "\x1b[35m",
+		"Green": "\x1b[32m",
+		"Cyan": "\x1b[36m",
+		"Black": "\x1b[30m",
+		"Grey": "\x1b[37m",
+		"White": "\x1b[37m",
+		"Reset": "\x1b[39m",
+		"Background_Red": "\x1b[41m",
+		"Background_Green": "\x1b[42m",
+		"Background_Yellow": "\x1b[43m",
+		"Background_Blue": "\x1b[44m",
+		"Background_Purple": "\x1b[45m",
+		"Background_Cyan": "\x1b[46m",
+		"Background_LightGrey": "\x1b[47m",
+		"Background_DarkGrey": "\x1b[100m",
+		"Background_LightRed": "\x1b[101m",
+		"Background_LightGreen": "\x1b[102m",
+		"Background_LightYellow": "\x1b[103m",
+		"Background_Reset": "\x1b[49m",
+		"Clear": "\033[2J\033[1;1H"
+	}
