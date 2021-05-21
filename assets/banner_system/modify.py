ee220e31ebffbd9cd5c0504cce00ed98d51e1d44
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-21 14:48:11 -0400

    Update modify.py

--- assets/banner_system/modify.py
+++ assets/banner_system/modify.py
@@ -1,4 +1,4 @@
-from assets.Config.main import MainColors
+from assets.Config.main import Strings
 import sys, os, time
 
 
@@ -19,6 +19,26 @@ class BannerFunc():
     IPAddr = None
 
     def ColorBanner(bnnr):
-        bnnr = bnnr.replace("{RED}", MainColors['RED'])
+        bnnr = bnnr.replace("{RED}", Strings.MainColors['Red'])
+        bnnr = bnnr.replace("{YELLOW}", Strings.MainColors['Yellow'])
+        bnnr = bnnr.replace("{BLUE}", Strings.MainColors['Blue'])
+        bnnr = bnnr.replace("{PURPLE}", Strings.MainColors['Purple'])
+        bnnr = bnnr.replace("{GREEN}", Strings.MainColors['Green'])
+        bnnr = bnnr.replace("{BLACK}", Strings.MainColors['Black'])
+        bnnr = bnnr.replace("{GREY}", Strings.MainColors['Grey'])
+        bnnr = bnnr.replace("{CYAN}", Strings.MainColors['Cyan'])
+        bnnr = bnnr.replace("{WHITE}", Strings.MainColors['White'])
+        bnnr = bnnr.replace("{BG_GREY}", Strings.MainColors['Background_Red'])
+        bnnr = bnnr.replace("{BG_GREEN}", Strings.MainColors['Background_Green'])
+        bnnr = bnnr.replace("{BG_YELLOW}", Strings.MainColors['Background_Yellow'])
+        bnnr = bnnr.replace("{BG_BLUE}", Strings.MainColors['Background_Blue'])
+        bnnr = bnnr.replace("{BG_PURPLE}", Strings.MainColors['Background_Purple'])
+        bnnr = bnnr.replace("{BG_CYAN}", Strings.MainColors['Background_Cyan'])
+        bnnr = bnnr.replace("{BG_LIGHTGREY}", Strings.MainColors['Background_LightGrey'])
+        bnnr = bnnr.replace("{BG_DARKGREY}", Strings.MainColors['Background_DarkGrey'])
+        bnnr = bnnr.replace("{BG_LIGHTRED}", Strings.MainColors['Background_LightRed'])
+        bnnr = bnnr.replace("{BG_LIGHTGREEN}", Strings.MainColors['Background_LightGreen'])
+        bnnr = bnnr.replace("{BG_LIGHTYELLOW}", Strings.MainColors['Background_LightYellow'])
+        bnnr = bnnr.replace("{BG_RESET}", Strings.MainColors['Background_Reset'])
 
     
