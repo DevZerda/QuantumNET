b975866fc71ffd9c6f0cb048d36c664ede7cf15f
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-22 11:05:25 -0400

    Update main.py

--- assets/Config/main.py
+++ assets/Config/main.py
@@ -6,30 +6,30 @@ class Strings():
 		if name:
 			addName = "User: " + name
 		
-		return "[Quantum@" + addName + "]#~ "
+		return "\r[Quantum@" + addName + "]#~ "
 
 	MainColors = {
-		"Red": "\x1b[31m",
-		"Yellow": "\x1b[33m",
-		"Blue": "\x1b[34m",
-		"Purple": "\x1b[35m",
-		"Green": "\x1b[32m",
-		"Cyan": "\x1b[36m",
-		"Black": "\x1b[30m",
-		"Grey": "\x1b[37m",
-		"White": "\x1b[37m",
-		"Reset": "\x1b[39m",
-		"Background_Red": "\x1b[41m",
-		"Background_Green": "\x1b[42m",
-		"Background_Yellow": "\x1b[43m",
-		"Background_Blue": "\x1b[44m",
-		"Background_Purple": "\x1b[45m",
-		"Background_Cyan": "\x1b[46m",
-		"Background_LightGrey": "\x1b[47m",
-		"Background_DarkGrey": "\x1b[100m",
-		"Background_LightRed": "\x1b[101m",
-		"Background_LightGreen": "\x1b[102m",
-		"Background_LightYellow": "\x1b[103m",
-		"Background_Reset": "\x1b[49m",
+		"Red": "\e[31m",
+		"Yellow": "\e[33m",
+		"Blue": "\e[34m",
+		"Purple": "\e[35m",
+		"Green": "\e[32m",
+		"Cyan": "\e[36m",
+		"Black": "\e[30m",
+		"Grey": "\e[37m",
+		"White": "\e[37m",
+		"Reset": "\e[39m",
+		"Background_Red": "\e[41m",
+		"Background_Green": "\e[42m",
+		"Background_Yellow": "\e[43m",
+		"Background_Blue": "\e[44m",
+		"Background_Purple": "\e[45m",
+		"Background_Cyan": "\e[46m",
+		"Background_LightGrey": "\e[47m",
+		"Background_DarkGrey": "\e[100m",
+		"Background_LightRed": "\e[101m",
+		"Background_LightGreen": "\e[102m",
+		"Background_LightYellow": "\e[103m",
+		"Background_Reset": "\e[49m",
 		"Clear": "\033[2J\033[1;1H"
 	}
