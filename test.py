6bd0d9473e2d2bbeceb49234303c4a087df392e6
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-22 14:01:56 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from assets.Auth.crud import *
+from assets.Auth.crudFunc import *
 
 # print(CRUD.GetUser("root"))
 
-print(CRUD.RemoveUser(sys.argv[1]))
+print(CrudFunctions.isPremium("root"))
+# print(CRUD.RemoveUser(sys.argv[1]))
 
