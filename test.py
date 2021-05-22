4a3716ab6079feb70e7768131a5107d7be04e747
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-22 15:17:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from assets.Auth.crud import *
 from assets.Auth.crudFunc import *
+from assets.banner_system.modify import *
+
+print(BannerModify.GetBannerFromFile("help"))
 
 # print(CRUD.GetUser("root"))
 
-print(CrudFunctions.isPremium("root"))
+# print(CrudFunctions.isPremium("root"))
+
 # print(CRUD.RemoveUser(sys.argv[1]))
 
