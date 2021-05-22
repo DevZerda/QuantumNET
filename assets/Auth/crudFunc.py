c467af9c72256054c448c707a0055e5989140104
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-22 14:01:21 -0400

    Update crudFunc.py

--- assets/Auth/crudFunc.py
+++ assets/Auth/crudFunc.py
@@ -1,9 +1,19 @@
 import os, sys, time
+from .crud import *
 
 class CrudFunctions():
+    def isRegistered(username):
+        info = CRUD.GetUser(username)
     
-    def isPremium():
-        return ""
+    def isPremium(username):
+        info = CRUD.GetUser(username)
+        if info == "[x] Error, No user found!":
+            return "[x] Error, No user found!\r\n (This seems like a buggy function)"
 
-    def isReseller():
-        return ""
+        info = info.split(",")
+        if int(info[3]) == 0:
+            return False
+        elif int(info[3]) > 0 & int(info[3]) <= 5:
+            return True
+        else:
+            return False
