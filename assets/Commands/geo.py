800b2389f05915e34715c97b0083d7a658885433
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-05-23 13:09:28 -0400

    Update geo.py

--- assets/Commands/geo.py
+++ assets/Commands/geo.py
@@ -1,9 +1,12 @@
 import os, sys, time, requests, json
 
 def geo_command(socket, argv):
-    jsonResp = (requests.get("http://extreme-ip-lookup.com/json/" + argv[1]).text)
-    fix = jsonResp.replace("\",\"", "\r\n")
-    fix2 = fix.replace("\"", "")
-    fix3 = fix2.replace("{", "")
-    responsee = fix3.replace("}", "")
-    socket.send(f"{responsee}\r\n".encode("utf-8"))
+    if len(argv) == 2:
+        jsonResp = (requests.get("http://extreme-ip-lookup.com/json/" + argv[1]).text)
+        fix = jsonResp.replace("\",\"", "\r\n")
+        fix2 = fix.replace("\"", "")
+        fix3 = fix2.replace("{", "")
+        responsee = fix3.replace("}", "")
+        socket.send(f"{responsee}\r\n".encode("utf-8"))
+    else:
+        socket.send("[x] Error, Invalid Arugment\r\nUsage: geo <ip>")
