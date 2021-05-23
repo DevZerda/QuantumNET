7a2f7d71f1e4d47f7ccdd3092cf9d62391985248
Beta security <54654703+BetaCyberSec@users.noreply.github.com>
2021-05-23 12:23:59 -0400

    Update cf.py

--- assets/Commands/cf.py
+++ assets/Commands/cf.py
@@ -1,6 +1,6 @@
 import requests, sys
+from .Config.current import *
 
 def CF():
-    send = requests.get('https://webresolver.nl/api.php?key=M8GAR-4LBHP-I3WD8-S1Y0T&html=0&action=cloudflare&string=' + sys.argv[1])
-    print(send.text)
-CF()
+    if len(Current.CurrentCmd[args]) == 2:
+        socket.send('https://webresolver.nl/api.php?key=M8GAR-4LBHP-I3WD8-S1Y0T&html=0&action=cloudflare&string='.encode('utf-8'))
