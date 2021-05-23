f4e78e6e9418f52e24bef86f14e52b7ac46d6a39
Beta security <54654703+BetaCyberSec@users.noreply.github.com>
2021-05-23 14:40:23 +0000

    Add files via upload

---
+++ assets/Commands/cf.py
@@ -0,0 +1,6 @@
+import requests, sys
+
+def CF():
+    send = requests.get('https://webresolver.nl/api.php?key=M8GAR-4LBHP-I3WD8-S1Y0T&html=0&action=cloudflare&string=' + sys.argv[1])
+    print(send.text)
+CF()
