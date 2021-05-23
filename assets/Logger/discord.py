243a94cdd57372062d49f04034bed9fdf78003d3
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-05-23 14:16:19 -0400

    Update discord.py

--- assets/Logger/discord.py
+++ assets/Logger/discord.py
@@ -3,33 +3,27 @@ import os, sys, time
 from discord_webhook import DiscordWebhook
 
 class Discord:
-	# webhooks = {
-	# 	"screen": "https://discord.com/api/webhooks/844855350801072159/xPsomucP4neo4dOdGLBaIbRmz8ldvCr9617c2FcWgDjrwrvuemlc2shLtl5TJCjq6zi7",
-	# 	"login": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
-	# 	"action": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1",
-	# 	"attack": "https://discord.com/api/webhooks/844148531052412968/_uiWklaFSMI79_3NcKt45ZKumn6hvktS-mUSDnwxp_H630EIK8brKi74WWdcET_6HmH1"
-	# } # Exo
+	def send_news(msg):
+		msg = f"```{msg}```"
+		webhook = DiscordWebhook(url='https://discord.com/api/webhooks/846082323158138881/l6AGbp9UMQqxeeetqzi5QHDkug1ftcdCiUFSkPynSrDkxZgBJzLGM-v4-aBz2tVoaLBm', content=msg)
+		webhook.execute()
 
-	def send_notice(msg):
-		# types = ["screen", "login", "attack", "action"]
-		# if type in types:
+	def send_status(msg):
+		msg = f"```{msg}```"
 		webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/844855350801072159/xPsomucP4neo4dOdGLBaIbRmz8ldvCr9617c2FcWgDjrwrvuemlc2shLtl5TJCjq6zi7', content=msg)
 		webhook.execute()
-		# else: 
-			# print("Unable to send request!")
 
+	def send_attack(msg):
+		msg = f"```{msg}```"
+		webhook = DiscordWebhook(url='https://discord.com/api/webhooks/846080791829282846/WokclMPaJ8G3UGtS5huyMvqbAPDobwJDq-K_B4Nxe5khoJKTwPt9wB2m_FllKTbQcPa6', content=msg)
+		webhook.execute()
 
-class DiscordFunc():
-	def netStartUp(host, port, time):
-		notice = f"Quantum NET has successfully started\n\nHost: {host} | Port: {port}\nTime: {time}"
-		Discord.send_notice(notice)
+	def send_login(msg):
+		msg = f"```{msg}```"
+		webhook = DiscordWebhook(url='https://discord.com/api/webhooks/846081409659699211/dSkGHSLzYIrPrBcwqdfzBUqRKAkzme2Ibae0Wm-fjA6pmcRxzoy1naPYrf0Am5ItU7Mw', content=msg)
+		webhook.execute()
 
-	def LoginLog(username, time):
-		notice = f"[+] Login Attempted!\n\nUser: {username} | Time: {time}"
-		Discord.send_notice(notice)
-
-	def LogAttack(username, ip, port, time):
-		notice = ""
-
-	def LogCmd():
-		notice = ""
+	def send_logs(msg):
+		msg = f"```{msg}```"
+		webhook = DiscordWebhook(url='https://discord.com/api/webhooks/844893815772282880/r20oO4fg-CkbmxLs_QhsmS0AHe_kQ4BzkvfTTYZx9Zmp7SKQqBRDCUfTDcUK5dsPC8-L', content=msg)
+		webhook.execute()
