f6edb06d766e7768326ef769930cd73d76635092
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-05-22 22:44:49 -0400

    Update main.py

--- assets/Logger/main.py
+++ assets/Logger/main.py
@@ -5,13 +5,15 @@ import os, sys, time
 from ..Config.main import *
 from ..Config.current import *
 from ..utils.main import *
+from .discord import *
 
 class MainLogger:
-    def Log(logType):
-        logResp = "Log: " + logType + " | Time: " + utils.CurrentTime() + "\n"
-        logResp += "User: " + Current.CurrentInfo['Username'] + " | IP: " + Current.CurrentInfo['IP'] + "\n"
-        logResp += "Level: " + Current.CurrentInfo['Level'] + " | Admin: " + Current.CurrentCmd['Admin'] + "\n"
-        logResp += "Cmd: " + Current.CurrentCmd['Cmd'] + " | Full CMD: " + Current.CurrentCmd['Fullcmd'] + "\n"
+    def Log(logType, notification):
+        logResp = "Log: " + logType + " | Time: " + str(utils.CurrentTime()) + "\n"
+        if "CMD" in logType:
+            logResp += "User: " + Current.CurrentInfo['Username'] + " | IP: " + Current.CurrentInfo['IP'] + "\n"
+            logResp += "Level: " + str(Current.CurrentInfo['Level']) + " | Admin: " + str(Current.CurrentInfo['Admin']) + "\n"
+            logResp += "Cmd: " + Current.CurrentCmd['Cmd'] + " | Full CMD: " + Current.CurrentCmd['fullcmd'] + "\n"
         print(logResp)
 
         LogTypes.LogCommand(logResp)
@@ -20,20 +22,27 @@ class MainLogger:
         elif logType == "login":
             LogTypes.LogLogin(logResp)
 
+        if notification == True:
+            Discord.send_notice(logResp)
+
 
 class LogTypes:
+
+    def test():
+        print(os.path.dirname(__file__), "/../")
     def LogCommand(logThis):
-        logsDB = open("../db/logs.db", "a")
+        logsDB = open(os.getcwd() + "/assets/db/logs.db", "a")
         logsDB.write(logThis)
         logsDB.close()
 
     def LogAttack(logThis):
-        attkDB = open("../db/attacks.db", "a")
+        attkDB = open("./db/attacks.db", "a")
         attkDB.write(logThis)
         attkDB.close()
+        
 
     def LogLogin(logThis):
-        LoginDB = open("../db/logins.db", "a")
+        LoginDB = open("./db/logins.db", "a")
         LoginDB.write(logThis)
         LoginDB.close()
 
