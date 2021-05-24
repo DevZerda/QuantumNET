77f99ff51839e08fc032aaa7aab2f323d687ed27
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-05-23 20:16:54 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from assets.utils.main import *
+
+# print(BannerModify.GetBannerFromFile("help"))
+
+# print(CRUD.GetUser("root"))
+
+# print(CrudFunctions.isPremium("root"))
+
+# print(CRUD.RemoveUser(sys.argv[1]))
+
+test = utils.arr2str(['testing', 'this'], sys.argv[1])
+print(test)
