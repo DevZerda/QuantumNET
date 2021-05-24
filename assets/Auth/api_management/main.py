0bfe03ea0693773fabb15eb36d064c6cb1943c9e
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-05-24 18:47:47 -0400

    Delete main.py

--- assets/Auth/api_management/main.py
+++
@@ -1,9 +0,0 @@
-# Modules
-import os, sys, time, requests
-
-# Files
-from ..Config.main import *
-
-class API_Manager:
-    def addAPI(api, methods):
-        apiDB = open("./assets/db/apis.db", "r").read()
