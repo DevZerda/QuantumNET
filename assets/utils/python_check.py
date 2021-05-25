9ce45e86cd97cc7254ee482740d7ee8a6badd192
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-25 08:01:32 -0400

    Create python_check.py

---
+++ assets/utils/python_check.py
@@ -0,0 +1,23 @@
+import os, sys, time, subprocess
+
+class PythonCheck:
+    def LinuxPython3Check():
+        check = subprocess.getoutput("python3")
+        if "command not found" in check:
+            return False
+        else:
+            return True
+
+    def LinuxPip3Check():
+        check = subprocess.getoutput("pip3")
+        if "command not found" in check:
+            return False
+        else:
+            return True
+
+class PythonInstalls():
+    def installPython3():
+        os.system("sudo yum install python3")
+        os.system("sudo apt install python3")
+        os.system("sudo yum install pip3")
+        os.system("sudo apt install pip3")
