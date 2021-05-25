52529e2fcbf0a3feb5c3b9d3d4eccf96124942c0
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-05-24 20:13:46 -0400

    Delete get_usage.py

--- cnc_control/get_usage.py
+++
@@ -1,7 +0,0 @@
-import os, sys, time, subprocess
-
-class Usage:
-    def Usage():
-        cnc_pid = subprocess.getoutput("pgrep -f c2.py").split("\n")[0]
-        get_usage = subprocess.getoutput(f"ps -p {cnc_pid} -o %cpu,%mem,cmd").split(" ")
-        print("CPU: " + get_usage[3] + " | MEM: " + get_usage[5])
