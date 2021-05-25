879a9bc9c24f45804f2adef1a75624709665e8c7
Exo0001 <84390579+Exo0001@users.noreply.github.com>
2021-05-25 17:33:10 +0100

    Update cf.py

--- assets/Commands/cf.py
+++ assets/Commands/cf.py
@@ -1,5 +1,8 @@
 import requests, sys
 
-def CF():
-    send = requests.get('https://webresolver.nl/api.php?key=M8GAR-4LBHP-I3WD8-S1Y0T&html=0&action=cloudflare&string=' + sys.argv[1])
-    # print(send.text)
+def cloudflare_resolve(socket):
+	socket.send("\rHost: ".encode("utf-8"))
+	backend = socket.recv(512).decode("utf-8")
+	backend = socket.recv(1024).decode("utf-8")
+	response = requests.get("https://webresolver.nl/api.php?key=M8GAR-4LBHP-I3WD8-S1Y0T&html=0&action=cloudflare&string=" + backend)
+	socket.send(str(response.text).encode("utf-8"))
