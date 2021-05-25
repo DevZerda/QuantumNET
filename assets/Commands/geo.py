89d4991793a6299227428c87199aeac35a826e96
Steven <scared@tuta.io>
2021-05-25 14:52:05 -0400

    Fix bad code

--- assets/Commands/geo.py
+++ assets/Commands/geo.py
@@ -1,12 +1,16 @@
-import os, sys, time, requests, json
+import os
+import sys
+import time
+import requests
+import json
+
 
 def geo_command(socket, argv):
     if len(argv) == 2:
-        jsonResp = (requests.get("http://extreme-ip-lookup.com/json/" + argv[1]).text)
+        jsonResp = (requests.get(
+            "http://extreme-ip-lookup.com/json/" + argv[1]).text)
         fix = jsonResp.replace("\",\"", "\r\n")
-        fix2 = fix.replace("\"", "")
-        fix3 = fix2.replace("{", "")
-        responsee = fix3.replace("}", "")
-        socket.send(f"{responsee}\r\n".encode("utf-8"))
+        fix = fix.replace("\"", "").replace("{", "").replace("}", "")
+        socket.send(f"{fix}\r\n".encode("utf-8"))
     else:
         socket.send("[x] Error, Invalid Arugment\r\nUsage: geo <ip>")
