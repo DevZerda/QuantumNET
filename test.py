f63086c0d1535386a9377eb4ac0ed2abef94a120
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-25 08:01:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-from cnc_control.get_usage import *
-
-Usage.Usage()
+import os, time, time
+print(os.path.isfile("assets/banner_system/banners/main.txt"))
+f = open("assets/banner_system/banners/main.txt", "r").read()
+print(f)
