66323f6bd0058b83b316f59efaf8f486ce1ddb11
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-25 10:24:46 -0400

    Update start.py

---
+++ start.py
@@ -0,0 +1,16 @@
+# Modules
+import os, sys, time
+
+# Files
+from assets.utils.main import *
+
+
+
+def Check4Modules():
+    return ""
+
+
+def InstallModules():
+    if utils.GetOS() == True:
+        ## Check for pip3
+        return ""
