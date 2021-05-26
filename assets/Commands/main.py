b4718bd4d6865be8a4427391931fdd08984ef79b
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-26 19:16:14 -0400

    Update main.py

--- assets/Commands/main.py
+++ assets/Commands/main.py
@@ -19,22 +19,24 @@ def MainScreen(socket, ip):
         free_mode(socket, ip)
     elif int(option_dude) == 4:
         about(socket)
+    else:
+        socket.send("[x] Error, Invalid Argument\r\n".encode())
+
 
 
 def login(socket, ip):
     # User Input Login Section
     socket.send("Username: ".encode())
     username = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
+    socket.recv(buffer_length)
     socket.send("Password: ".encode())
     password = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
 
-    socket.send("Welcome to Quantum Net\r\n".encode())
-
 # Login Check
     if "[+]" in Auth.Login(username, password, ip): # This is a weird way of authentication lol 
-        socket.send(f"Welcome: {username}\r\n".encode())
         Current.CurrentInfo['Username'] = username
         socket.send(str(Strings.MainColors['Clear'] + BannerModify.GetBannerFromFile("main") + "\r\n" + BannerModify.GetBannerFromFile("net_stats")).encode())
+        socket.send(f"Welcome to Quantum Net {username}\r\n".encode())
         MainLogger.Log(f"login: {username} | {password} {utils.CurrentTime()}", True)
         return username
     else:
