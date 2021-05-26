f77d36c74d5129bae164b385941b17caa0e1f2ec
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-26 09:06:36 -0400

    Update crudFunc.py

--- assets/Auth/crudFunc.py
+++ assets/Auth/crudFunc.py
@@ -1,7 +1,7 @@
 import os, sys, time
 from .crud import *
 
-class CrudFunctions():
+class CrudFunctions:
     def ChangePW(user, newpw):
         info = CRUD.GetUser(user).split(",")
         CRUD.RemoveUser(user)
@@ -13,6 +13,10 @@ class CrudFunctions():
 
     def isRegistered(username):
         info = CRUD.GetUser(username)
+        if info == "[x] Error, No user found!":
+            return False
+
+        return True
     
     def isPremium(username):
         info = CRUD.GetUser(username)
@@ -23,6 +27,39 @@ class CrudFunctions():
         if int(info[3]) == 0:
             return False
         elif int(info[3]) > 0 & int(info[3]) <= 5:
+            return True
+        else:
+            return False
+
+    def isReseller(username):
+        info = CRUD.GetUser(username)
+        if info == "[x] Error, No user found!":
+            return "[x] Error, No user found!\r\n (This seems like a buggy function)"
+
+        info = info.split(",")
+        if int(info[5]) == 1:
+            return True
+        else:
+            return False
+
+    def isAdmin(username):
+        info = CRUD.GetUser(username)
+        if info == "[x] Error, No user found!":
+            return "[x] Error, No user found!\r\n (This seems like a buggy function)"
+
+        info = info.split(",")
+        if int(info[5]) == 2:
+            return True
+        else:
+            return False
+
+    def isOwner(username):
+        info = CRUD.GetUser(username)
+        if info == "[x] Error, No user found!":
+            return "[x] Error, No user found!\r\n (This seems like a buggy function)"
+
+        info = info.split(",")
+        if int(info[5]) == 3:
             return True
         else:
             return False
