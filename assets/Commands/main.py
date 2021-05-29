cb92bb2715112a4ea93348619db36ed3aeddd2bf
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-29 14:09:31 -0400

    Update main.py

--- assets/Commands/main.py
+++ assets/Commands/main.py
@@ -5,6 +5,7 @@ import os, sys, time
 from ..Auth.main import *
 from ..Logger.main import *
 from ..banner_system.modify import *
+from ..Config.main import *
 
 buffer_length = 1024
 
@@ -22,35 +23,39 @@ def MainScreen(socket, ip):
         about(socket)
     else:
         socket.send("[x] Error, Invalid Argument\r\n".encode())
+        MainScreen(socket,ip)
 
 
 
 def login(socket, ip):
+    socket.send(str(Strings.MainColors['Clear']).encode())
     # User Input Login Section
-    socket.send("Username: ".encode())
+    socket.recv(1024)
+    socket.send("\rUsername: ".encode())
     username = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
-    socket.recv(buffer_length)
-    socket.send("Password: ".encode())
+    socket.recv(1024)
+    socket.send("\rPassword: ".encode())
     password = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
     print(f"{username} | {password}")
 
 # Login Check
     if "[+]" in Auth.Login(username, password, ip): # This is a weird way of authentication lol 
         Current.CurrentInfo['Username'] = username
-        socket.send(str(Strings.MainColors['Clear'] + BannerModify.GetBannerFromFile("main") + "\r\n" + BannerModify.GetBannerFromFile("net_stats")).encode())
+        socket.send(str(Strings.MainColors['Clear'] + CustomBannerMaker.CreateMOTD(utils.GetMOTD()) + BannerModify.GetBannerFromFile("main") + "\r\n" + BannerModify.GetBannerFromFile("net_stats")).encode())
         socket.send(f"Welcome to Quantum Net {username}\r\n".encode())
         MainLogger.Log(f"login: {username} | {password} {utils.CurrentTime()}", True)
-        return username
+        Current.CurrentInfo['Username'] = username
     else:
         socket.send("[x] Error, Incorrect username or password. Try again....".encode())
         time.sleep(4)
         socket.close()
 
-def register(socket, ip):
+def register(socket, ip): ## Function not finished
     socket.send("Username: ".encode())
     username = socket.recv(buffer_length).decode().strip().replace("\r\n","")
     socket.send("Password: ".encode())
     password = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
+    Current.CurrentInfo['Username'] = username
 
 
 
