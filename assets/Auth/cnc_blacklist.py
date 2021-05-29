512e51eb4389a0b16bbcf002107de287f12f8500
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-29 13:31:38 -0400

    Create cnc_blacklist.py

---
+++ assets/Auth/cnc_blacklist.py
@@ -0,0 +1,14 @@
+# Modules
+import os, sys, time
+
+# Files
+
+class CNCBlacklist:
+    def Check4IP(ip):
+        return ""
+
+    def addIP(ip):
+        return ""
+
+    def removeIP(ip):
+        return ""
