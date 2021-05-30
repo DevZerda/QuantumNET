18323465ee6250dfe232117f9ef5a49f7ac0a04c
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-05-30 19:38:35 -0400

    Update main.py

--- assets/Commands/main.py
+++ assets/Commands/main.py
@@ -32,22 +32,21 @@ def login(socket, ip):
     # User Input Login Section
     socket.send("\rUsername: ".encode())
     username = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
-    socket.recv(1024)
+    # socket.recv(1024)
     socket.send("\rPassword: ".encode())
     password = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
     print(f"{username} | {password}")
 
 # Login Check
     if "[+]" in Auth.Login(username, password, ip): # This is a weird way of authentication lol 
-        Current.CurrentInfo['Username'] = username
         socket.send(str(Strings.MainColors['Clear'] + CustomBannerMaker.CreateMOTD(utils.GetMOTD()) + BannerModify.GetBannerFromFile("main") + "\r\n" + BannerModify.GetBannerFromFile("net_stats")).encode())
         socket.send(f"Welcome to Quantum Net {username}\r\n".encode())
         MainLogger.Log(f"login: {username} | {password} {utils.CurrentTime()}", True)
-        Current.CurrentInfo['Username'] = username
     else:
         socket.send("[x] Error, Incorrect username or password. Try again....".encode())
         time.sleep(4)
         socket.close()
+    return username
 
 def register(socket, ip): ## Function not finished
     socket.send("Username: ".encode())
